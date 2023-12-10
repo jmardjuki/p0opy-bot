4d54aef7bc08cd7776d176e472a0864a621e3b63
Janet Mardjuki <janetmardjuki@gmail.com>
2023-12-10 02:11:45 -0800

    Minimum message

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,6 +3,8 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
-		console.log(message);
+		console.log(message.author.id);
+		console.log(message.author.username);
+		console.log(message.content);
 	},
 };
